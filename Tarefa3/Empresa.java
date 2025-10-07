626b9790d7444a12f0fdef8937a0e7ef010b47e2
nvalbuquerque <nathalialyra@ufpr.br>
2025-10-07 16:52:33 -0300

    Update Empresa.java

--- Tarefa3/Empresa.java
+++ Tarefa3/Empresa.java
@@ -62,3 +62,76 @@ public class Empresa {
 		}
 	}
 }
+
+
+//////////////////// ESSE?
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Empresa {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+	    String[] nome = new String[10];
+	    double[] salario = new double[10];
+	    double soma = 0;
+	    boolean salarioValido = false;
+	    String nomeTemp = "";
+
+
+        for (int i = 0; i <10; i++) {
+		    
+		    while (nomeTemp.trim().isEmpty() || nomeTemp.length() < 3) {
+		        System.out.println("Digite o nome do funcionário:");
+			    nomeTemp = scanner.nextLine();
+			    
+			    if (nomeTemp.trim().isEmpty()) {
+			        System.out.println("Nome vazio.");
+			    } else if (nomeTemp.length() < 3) {
+			        System.out.println("O nome deve ter pelo menos 3 caracteres.");
+			    }
+		    }
+			    
+			nome[i] = nomeTemp;
+			
+			while (salarioValido == false) {
+			    try {
+		            System.out.println("Digite o salário do funcionário:");
+        			salario[i] = scanner.nextDouble();
+        			scanner.nextLine();
+        			
+        			if (salario[i] > 0) {
+        			    salarioValido = true;
+        			} else {
+        			    System.out.println("Salário inválido.");
+        			}
+        			
+        			soma += salario[i];
+        			
+		        } catch (InputMismatchException e) {
+		            System.out.println("Entrada de dados inválida.");
+		            scanner.nextLine();
+		        }
+			}
+        }
+		
+		double media = soma / 10;
+		System.out.printf("A média salarial dos funcionários é de R$ %.2f.\n", media);
+		
+		System.out.println("Os funcionários com salário acima da média são:");
+		
+		boolean controle = false;
+		
+		for (int i = 0; i < 10; i++) {
+		    if (salario[i] > media) {
+		       System.out.printf("%s - R$ %.2f\n", nome[i], salario[i]);
+		       controle = true;
+		    }
+		    
+		if (controle = false) {
+		    System.out.println("Nenhum funcinário possui salário acima da média.");
+		}
+		}
+    }
+}
