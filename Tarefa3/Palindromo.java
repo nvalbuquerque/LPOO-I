729cf49ea0ffdddd5b4a1c7266565a249654c76c
nvalbuquerque <nathalialyra@ufpr.br>
2025-10-20 16:53:23 -0300

    Finaliza
    Refactor Palindromo class to check for palindromes correctly.

--- Tarefa3/Palindromo.java
+++ Tarefa3/Palindromo.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
-public class Palindromo
-{
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String frase = "";
-		
-		System.out.println("Escreve uma frase e verifique se é um palíndromo:");
-		frase = scanner.nextLine();
-		
-		char fraseChar = frase.toCharArray();
-		
-		int inicio = 0;
-		int fim = fraseChar.length() - 1;
-		
-		System.out.print(fim);
-		
-		
-	}
+public class Palindromo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Insira uma frase: ");
+        String frase = scanner.nextLine();
+        
+        char[] letras = frase.replaceAll("[\\p{Punct}\\s]", "").toLowerCase().toCharArray();
+        
+        System.out.println("Frase limpa: " + new String(letras));
+        
+        int inicio = 0;
+        int fim = letras.length - 1;
+        boolean ehPalindromo = true;
+        
+        while (inicio < fim) {
+            if (letras[inicio] != letras[fim]) {
+                System.out.println("A frase não é um palíndromo.");
+                ehPalindromo = false;
+                break;
+            }
+            
+            inicio++;
+            fim--;
+        }
+        
+        if (ehPalindromo) {
+            System.out.println("A frase é um palíndromo!");
+        }
+    }
 }
