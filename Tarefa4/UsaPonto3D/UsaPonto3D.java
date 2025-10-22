2ba12ed8b7dd10bde513b3c82ab7d1f94843674b
nvalbuquerque <nathalialyra@ufpr.br>
2025-10-21 21:48:05 -0300

    Implement UsaPonto3D to interact with Ponto3D

---
+++ Tarefa4/UsaPonto3D/UsaPonto3D.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class UsaPonto3D {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Insira os dados do ponto 3D.");
+
+		System.out.println("Insira o valor de x:");
+		int x = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Insira o valor de y:");
+		int y = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Insira o valor de z:");
+		int z = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Insira a cor:");
+		String cor = scanner.nextLine();
+
+		System.out.println("Insira a intensidade:");
+		String intensidade = scanner.nextLine();
+
+		Ponto3D ponto = new Ponto3D(x, y, z, cor, intensidade);
+
+		System.out.println("Insira os dados do objeto.");
+
+		System.out.println("Insira o valor de x:");
+		int xObj = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Insira o valor de y:");
+		int yObj = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Insira o valor de z:");
+		int zObj = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Insira a cor:");
+		String corObj = scanner.nextLine();
+
+		System.out.println("Insira a intensidade:");
+		String intensidadeObj = scanner.nextLine();
+
+		Ponto3D pontoObj = new Ponto3D(xObj, yObj, zObj, corObj, intensidadeObj);
+
+		double distancia = ponto.calculaDistancia(pontoObj);
+
+		System.out.println("Dados do ponto inserido:");
+		System.out.printf("Ponto3D(%d, %d, %d) - Cor: %s, Intensidade: %s", ponto.x, ponto.y, ponto.z, ponto.cor, ponto.intensidade);
+		System.out.println("\nDados do objeto inserido:");
+		System.out.printf("Ponto3D(%d, %d, %d) - Cor: %s, Intensidade: %s", pontoObj.x, pontoObj.y, pontoObj.z, pontoObj.cor, pontoObj.intensidade);
+		System.out.printf("\nDistância entre os pontos: %.2f\n", distancia);
+		
+		System.out.println("Modificando o ponto com os sets:");
+        ponto.setX(10);
+        ponto.setY(20);
+        ponto.setZ(30);
+        ponto.setCor("Verde");
+        ponto.setIntensidade("Baixa");
+        
+        System.out.println("Ponto modificado:");
+        System.out.printf("Ponto3D(%d, %d, %d) - Cor: %s, Intensidade: %s", ponto.x, ponto.y, ponto.z, ponto.cor, ponto.intensidade);
+
+        double distanciaModificada = ponto.calculaDistancia(pontoObj);
+        System.out.printf("\nNova distância entre os pontos: %.2f\n\n", distanciaModificada);
+		
+	}
+}
