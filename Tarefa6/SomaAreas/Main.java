dad4f7538a1f639518cac5784041f63c520e4b15
Nathalia Lyra Varela de Albuquerque <nathalialyra@ufpr.br>
2025-11-18 13:08:01 -0300

    Refactor area calculations using array of shapes

--- Tarefa6/SomaAreas/Main.java
+++ Tarefa6/SomaAreas/Main.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
+		Superficie[] superficies = new Superficie[3];
 
 		System.out.println("Insira o valor do raio da circunferencia:");
 		double raio = scanner.nextDouble();
 
-		Circunferencia c1 = new Circunferencia(raio);
+		superficies[0] = new Circunferencia(raio);
 		
 		System.out.println("Insira o valor do lado do quadrado:");
 		double lado = scanner.nextDouble();
 		
-		Quadrado q1 = new Quadrado(lado);
+		superficies[1] = new Quadrado(lado);
 		
 		System.out.println("Insira o valor do base do triangulo:");
 		double base = scanner.nextDouble();
@@ -20,35 +22,30 @@ public class Main {
 		System.out.println("Insira o valor da altura do triangulo:");
 		double altura = scanner.nextDouble();
 		
-		Triangulo t1 = new Triangulo(base, altura);
+		superficies[2] = new Triangulo(base, altura);
 		
-		double areaC1 = c1.calculaArea();
-		double areaQ1 = q1.calculaArea();
-		double areaT1 = t1.calculaArea();
+		double areaC1 = superficies[0].calculaArea();
+		double areaQ1 = superficies[1].calculaArea();
+		double areaT1 = superficies[2].calculaArea();
 		
-		double perimetroQ1 = q1.perimetro();
+		double perimetroQ1 = ((Quadrado)superficies[1]).perimetro();
 
 		System.out.println("Informacoes da circunferencia:");
-		System.out.printf("Raio: %.2f", c1.raio);
+		System.out.printf("Raio: %.2f", ((Circunferencia)superficies[0]).raio);
 		System.out.printf("\nArea: %.2f", areaC1);
 		
 		System.out.println("\nInformacoes do quadrado:");
-		System.out.printf("Lado: %.2f", q1.lado);
+		System.out.printf("Lado: %.2f", ((Quadrado)superficies[1]).lado);
 		System.out.printf("\nPerimetro: %.2f", perimetroQ1);
 		System.out.printf("\nArea: %.2f", areaQ1);
 		
 		System.out.println("\nInformacoes do triangulo:");
-		System.out.printf("Base: %.2f", t1.base);
-		System.out.printf("\nAltura: %.2f", t1.altura);
+		System.out.printf("Base: %.2f", ((Triangulo)superficies[2]).base);
+		System.out.printf("\nAltura: %.2f", ((Triangulo)superficies[2]).altura);
 		System.out.printf("\nArea: %.2f", areaT1);
 	    
-	    double areasExemplo = somaAreas(areaC1, areaQ1, areaT1);
+	    double areasExemplo = SomaAreas.somaAreas(superficies);
 	    
 	    System.out.printf("\nO resultado da soma das areas da circunferencia, do quadrado e do triangulo e de: %.2f", areasExemplo);
-	}
-	
-    public static double somaAreas(double area1, double area2, double area3) {
-        double area = area1 + area2 + area3;
-        return area;
 	}
 }
