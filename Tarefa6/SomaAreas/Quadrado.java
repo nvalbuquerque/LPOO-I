c80a75037d24c67d2f6f98b28ba635dfa2bf4985
Nathalia Lyra Varela de Albuquerque <nathalialyra@ufpr.br>
2025-11-18 11:37:25 -0300

    Add Quadrado class to calculate area and perimeter

---
+++ Tarefa6/SomaAreas/Quadrado.java
@@ -0,0 +1,18 @@
+public class Quadrado implements Superficie {
+	public double lado;
+
+	public Quadrado(double lado) {
+		this.lado = lado;
+	}
+	
+	@Override
+	public double calculaArea() {
+	    double area = lado*lado;
+	    return area;
+	}
+		
+	public double perimetro() {
+	    double perimetro = lado*4;
+	    return perimetro;
+	}
+}
